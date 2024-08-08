0a0d1bddd69485f2999f0e4d34099f621f9384d9
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-08 09:53:48 +0900

    feat : 비지니스 로직 수정 중
    도와줘 성호맨!!

--- src/TodoListService.java
+++ src/TodoListService.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TodoListService {
 
@@ -15,15 +12,37 @@ public class TodoListService {
 
     Map<String, List<String>> userMap = new HashMap<>();
     List<String> userList = List.of("미정", "미정", "미정", "미정", "미정");
+    Scanner sc = new Scanner(System.in);
 
-    public Map<String, List<String>> initUserTodoList(String userName) {
+    public void checkUserTodoList(String userName) {
+        if (!userMap.containsKey(userName)) {
+            createUserTodoList(userName);
+        }
+    }
+
+    public Map<String, List<String>> createUserTodoList(String userName) {
         userMap.put(userName, userList);
         return userMap;
     }
 
-    public void userTodoList(String userName) {
-        Map<String,List<String>> userTodoList= initUserTodoList(userName);
+    public void showTodoMenu(String userName) {
+        System.out.println("주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5.팀 멤버와의 1:1 면담");
+        System.out.println("Title 번호 : ");
+        int selectNum = sc.nextInt();
+
+
+        List<String> values=userMap.get(userName);
+        values.set(selectNum-1,"완료");
+
+        System.out.println("Status : 완료");
+
+        System.out.println(userMap.toString());
+
+
+
     }
+
+
 
     //성호님한테 받을 값
 }
