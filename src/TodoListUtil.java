febf1befee4bade989a07bc799d58a611b3f6871
ksngh <chzhqk98@naver.com>
2024-08-08 09:44:55 +0900

    Feat : I/O 처리 및 유저 인포 반환

--- src/TodoListUtil.java
+++ src/TodoListUtil.java
@@ -3,8 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TodoListUtil {
 
@@ -13,13 +11,35 @@ public class TodoListUtil {
 
 	public void beforeStart() throws IOException {
 		bw.write("Status 기호 : 1: 완료, 2: 미정 \r\n");
-		bw.flush();
-		bw.write("종료 여부 : c-계속 입력 ,q- 다음 참여자로 넘기기 ,x- 입력 종료");
+		bw.write("종료 여부 : c-계속 입력 ,q- 다음 참여자로 넘기기 ,x- 입력 종료 \r\n");
 		bw.flush();
 	}
 
 	public String extractUsername() throws IOException {
 		bw.write("Input Your Name:");
+		bw.flush();
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public int titleNumber() throws IOException {
+		try {
+			bw.write("To Do List 중 하나를 선택하세요!\r\n");
+			bw.flush();
+			bw.write("1. 주간 보고서 작성, 2. 이메일 확인 및 응답, 3. 회의 준비, 4. 프로젝트 계획서 수정, 5.팀 멤버와의 1:1 면담\r\n");
+			bw.flush();
+			return Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String choiceQCX()throws IOException {
+		bw.write("종료 여부:\r\n");
+		bw.flush();
 		try {
 			return br.readLine();
 		} catch (IOException e) {
